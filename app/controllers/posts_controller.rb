b3c4f23ae376283219006e48672dc08089a01ae5
ympon <67887018+ympon@users.noreply.github.com>
2020-07-29 20:07:38 +0900

    add-category

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,9 @@ class PostsController < ApplicationController
   def index
     # @posts = Post.new
   end
+
+  def get_category_children
+    @children = Category.find(params[:parent_id]).children
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
   has_many :posts_categories, dependent: :destroy
   has_many :categories, through: :posts_categories
 
-
   validates :image, presence: true, unless :text?
   validates :text, presence: true
 
