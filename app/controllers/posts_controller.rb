ce8428ed767a67bc9a671232447f7dffedfdb217
ympon <67887018+ympon@users.noreply.github.com>
2020-07-29 18:52:39 +0900

    prepatration-post-controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.new
+    # @posts = Post.new
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 
   validates :image, presence: true, unless :text?
   validates :text, presence: true
