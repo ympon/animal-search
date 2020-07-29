2eac83ad749d95aeeb955427c8b9e19015c984be
ympon <67887018+ympon@users.noreply.github.com>
2020-07-29 19:12:39 +0900

    add-association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_ancestry
+  has_many :posts_categories, dependent: :destroy
+  has_many :posts, through: :posts_categories
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   # belongs_to :user
+  has_many :posts_categories, dependent: :destroy
+  has_many :categories, through: :posts_categories
+
 
   validates :image, presence: true, unless :text?
   validates :text, presence: true
