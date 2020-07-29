207a9928ce4b40993920644d95c864d03891a717
ympon <67887018+ympon@users.noreply.github.com>
2020-07-29 20:32:38 +0900

    introduce-jquery

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :posts, only: [:index, :create]
+
+  resources :posts do
+    collection do # 新規用（new) usr:posts/newのため
+      get 'get_category_children', defaults: { format: 'json' }
+    end
+    member do # 編集(edit用) usl: posts/id/editのため
+      get 'get_category_children', defaults: { format: 'json' }
+    end
 end
