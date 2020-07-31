7cb0235c0291e1528676a222cb4f913fcc73644e
ympon <67887018+ympon@users.noreply.github.com>
2020-08-01 00:46:59 +0900

    add-edit-method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,10 @@ class PostsController < ApplicationController
     redirect_back(fallback_location: post_path)
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:image, :text)
