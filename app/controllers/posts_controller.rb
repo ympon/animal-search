b43388666746236100bb2f69719b9e01e694cebe
ympon <67887018+ympon@users.noreply.github.com>
2020-08-01 22:01:17 +0900

    logout-user-redirect-to-index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @posts = Post.all.order("created_at DESC")
     @post = Post.new
@@ -41,5 +44,11 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:image, :text)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
