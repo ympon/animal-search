79f10aa2b9f578ac717d38d9dd73233ad9c74c51
ympon <67887018+ympon@users.noreply.github.com>
2020-08-03 14:17:36 +0900

    reset-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_023447) do
+ActiveRecord::Schema.define(version: 2020_08_02_154808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,9 +51,8 @@ ActiveRecord::Schema.define(version: 2020_08_02_023447) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
-    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
